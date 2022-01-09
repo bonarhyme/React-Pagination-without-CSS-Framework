c01044fa5abd90ef1f25a387f7d9d89583edb2d3
Bonaventure <Bonarhyme@gmail.com>
2022-01-09 14:18:22 +0100

    Added starter files

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,31 +1,7 @@
 import React from "react";
 
-const Pagination = ({ page, pages, onClick, prevHandler, nextHandler }) => {
-  return (
-    <div>
-      <ul className="pages-container">
-        <li className="page-item" onClick={prevHandler}>
-          Prev
-        </li>
-
-        {[...Array(Math.ceil(pages)).keys()].map((x, i) => {
-          return (
-            <li
-              key={i}
-              className={page - 1 === i ? "active page-item" : "page-item"}
-              onClick={(e) => onClick(e.target.innerText)}
-            >
-              {x + 1}
-            </li>
-          );
-        })}
-
-        <li className="page-item" onClick={nextHandler}>
-          Next
-        </li>
-      </ul>
-    </div>
-  );
+const Pagination = () => {
+  return <div></div>;
 };
 
 export default Pagination;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,68 +1,7 @@
-import React, { useState } from "react";
-import { football } from "../variables/data";
-import Pagination from "./Pagination";
+import React from "react";
 
 const Table = () => {
-  const pageSize = 5;
-  const pageLength = football.length / pageSize;
-  const [page, setPage] = useState(1);
-  const [list, setList] = useState(football.slice(page - 1, page * pageSize));
-
-  const handlePage = (thePage) => {
-    setPage(thePage);
-    const t = football.slice((thePage - 1) * pageSize, thePage * pageSize);
-    setList(t);
-  };
-
-  const prevHandler = (thePage) => {
-    if (thePage === 1) {
-      handlePage(1);
-    } else {
-      handlePage(Number(thePage) - 1);
-    }
-  };
-
-  const nextHandler = (thePage, thePageLength) => {
-    if (thePage < thePageLength) {
-      handlePage(Number(thePage) + 1);
-    }
-  };
-
-  return (
-    <div className="table-container">
-      <table className="table">
-        <caption>Pagination in ReactJS without a CSS Framework</caption>
-        <thead>
-          <tr>
-            <th>Long</th>
-            <th className="center">Short</th>
-            <th>Country</th>
-          </tr>
-        </thead>
-        <tbody>
-          {list.map((x, i) => {
-            const { long, short, country } = x;
-            return (
-              <tr key={i}>
-                <td>{long}</td>
-                <td className="center">{short}</td>
-                <td>{country}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Pagination
-        page={page}
-        pages={pageLength}
-        onClick={(pageEvent) => {
-          handlePage(pageEvent);
-        }}
-        prevHandler={() => prevHandler(page)}
-        nextHandler={() => nextHandler(page, pageLength)}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Table;
